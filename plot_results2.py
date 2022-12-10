ecd4552abe72d739d80f15cccd9b9058e6c9b30c
anshumansinha16 <sinha.anshuman16@gmail.com>
2022-12-10 18:39:19 -0500

    ..

--- plot_results2.py
+++ plot_results2.py
@@ -60,13 +60,13 @@ Z = 1
 
 for exponent_approx in range(1,11):
 
-    #if(exponent_approx>=1):
-    #    xt  = 2**(exponent_approx)+1
-    #    x_tr = np.append(x_tr, (2**exponent_approx)*Z*(xt +1))
-    #    #x_tr = np.append(x_tr, (2**exponent_approx - 2**3)*Z*(xt +1)) #(2**exponent_approx - 2**6)*
-    #else:
-    xt  = 2**(exponent_approx)+1
-    x_tr = np.append(x_tr, Z*(xt +1))
+    if(exponent_approx>=1):
+        xt  = 2**(exponent_approx)+1
+        x_tr = np.append(x_tr, (2**exponent_approx)*Z*(xt +1))
+        #x_tr = np.append(x_tr, (2**exponent_approx - 2**3)*Z*(xt +1)) #(2**exponent_approx - 2**6)*
+    else:
+        xt  = 2**(exponent_approx)+1
+        x_tr = np.append(x_tr, Z*(xt +1))
 
     #normalized_MSE[exponent_approx] = d['normalized_MSE']
 
@@ -87,15 +87,15 @@ for exponent_approx in range(1,11):
             normalized_MSE_NN[exponent_approx] = d['NN_MSEs_test'] 
             #normalized_MSE_NN_obs[exponent_approx] = d['NN_MSEs_train']
             zin = 2**(exponent_approx)+1
-            #if(exponent_approx>=1):
-            #    #x = ((2*10*zin-1)*neurons + neurons) + ((2*neurons-1)+1) + (((2*neurons-1)*neurons + neurons)*(b_layers-1))
-            #    x = (b_layers-1)*(2*neurons*neurons) + 2*neurons*(1+Z*zin)*zin
-            #    y_axs = np.append(y_axs, d['NN_MSEs_test'])
-            #    x_axs = np.append(x_axs, x)
-            #else:
-            x = (b_layers-1)*(2*neurons*neurons) + 2*neurons*(1+Z*zin)
-            y_axs = np.append(y_axs, d['NN_MSEs_test'])
-            x_axs = np.append(x_axs, x)
+            if(exponent_approx>=1):
+                #x = ((2*10*zin-1)*neurons + neurons) + ((2*neurons-1)+1) + (((2*neurons-1)*neurons + neurons)*(b_layers-1))
+                x = (b_layers-1)*(2*neurons*neurons) + 2*neurons*(1+Z*zin)*zin
+                y_axs = np.append(y_axs, d['NN_MSEs_test'])
+                x_axs = np.append(x_axs, x)
+            else:
+                x = (b_layers-1)*(2*neurons*neurons) + 2*neurons*(1+Z*zin)
+                y_axs = np.append(y_axs, d['NN_MSEs_test'])
+                x_axs = np.append(x_axs, x)
 
             
             #if(exponent_approx==4):
