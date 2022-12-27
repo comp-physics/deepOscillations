375db9e2238bdbaba1f1b771bfe8ac6f6cfe7470
anshumansinha16 <sinha.anshuman16@gmail.com>
2022-12-26 22:51:05 -0500

    ..

--- main.py
+++ main.py
@@ -185,10 +185,23 @@ else:
     
 
 # Define the Trapezoidal Integrating Function
+# def Integrate_funcs(x,y,samples):
+#    I = np.zeros((samples,))
+#    for i in range(0,samples):
+#        I[i] = np.trapz(y[:,i], x)
+#    return I
+
+# Define the Trapezoidal Integrating Function
 def Integrate_funcs(x,y,samples):
     I = np.zeros((samples,))
+    len(x)
+    dx = (x[len(x)-1] - x[0])/(len(x)-2)
+    
     for i in range(0,samples):
-        I[i] = np.trapz(y[:,i], x)
+      I_i = 0
+      for m in range(1,len(x)-1):
+        I_i+= y[m,i]*dx
+      I[i] = I_i
     return I
 
 # Integrate the functions
