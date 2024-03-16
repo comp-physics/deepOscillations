b952fa6145d157b46abfdbcbb33cc9d12e7f0319
Spencer Bryngelson <shb@gatech.edu>
2024-03-16 12:48:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 20 14:54:40 2022
 
-@author: ethanpickering
-@author (modifications only): anshumansinha16
-"""
 import numpy as np
 import scipy.special as sp
 import matplotlib.pyplot as plt
